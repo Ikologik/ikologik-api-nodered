96104851709ec8c047cc32d018fe76eecaed937a
AzureAD\ElienMaes <elien.maes@ikologik.com>
2022-04-05 10:57:47 +0200

    Clean up code

--- ikologik-api/ikologik-batch-trace.js
+++ ikologik-api/ikologik-batch-trace.js
@@ -1,37 +1,47 @@
+const IkologikApi = require("ikologik-api-nodejs");
+
 module.exports = function (RED) {
 
 	function IkologikApiBatchTraceNode(config) {
 		RED.nodes.createNode(this, config);
 
-		// Get configuration from node
-		this.customerId = config.batchTraceCustomerId;
-		this.installationId = config.batchTraceInstallationId;
-		this.batchId = config.batchTraceBatchId;
+		// Get configuration
+		this.name = config.name;
+		this.server = RED.nodes.getNode(config.server);
+		this.function = config.function;
+		this.customerId = config.customerId;
+		this.installationId = config.installationId;
+		this.batchId = config.batchId;
 		this.batchTraceId = config.batchTraceId;
-		this.function = config.batchTraceFunction;
-		this.credentials = RED.nodes.getNode(config.batchTraceCredentials);
-		var node = this;
 
-		// Functionality of node
+		// Functionality
+		var node = this;
 		node.on('input', async function (msg) {
-			// If input from node choose input, otherwise get info from message
+			// Get data
 			var customerId = node.customerId ? node.customerId : msg.customerId;
 			var installationId = node.installationId ? node.installationId : msg.installationId;
 			var batchId = node.batchId ? node.batchId : msg.batchId;
 			var batchTraceId = node.batchTraceId ? node.batchTraceId : msg.batchTraceId;
 
-			// Use Ikologik-api for requests
-			const api = await node.credentials.api;
-			if (node.function === 'batchTraceGetById') {
-				msg.payload = await api.batchTrace.getById(customerId, installationId, batchId, batchTraceId);
-			} else if (node.function === 'batchTraceList') {
-				msg.payload = await api.batchTrace.list(customerId, installationId, batchId);
-			} else if (node.function === 'batchTraceCreate') {
-				msg.payload = await api.batchTrace.create(customerId, installationId, batchId, msg.batchTrace);
-			} else if (node.function === 'batchTraceUpdate') {
-				msg.payload = await api.batchTrace.update(customerId, installationId, batchId, batchTraceId, msg.batchTrace);
+			// Execute
+			try{
+				if (node.function === 'batchTraceGetById') {
+					msg.payload = await node.server.api.batchTrace.getByCustomerAndInstallationAndId(customerId, installationId, batchId, batchTraceId);
+				} else if (node.function === 'batchTraceList') {
+					msg.payload = await node.server.api.batchTrace.listByCustomerAndInstallationAndBatch(customerId, installationId, batchId);
+				} else if (node.function === 'batchTraceCreate') {
+					msg.payload = await node.server.api.createByCustomerAndInstallationAndBatch(customerId, installationId, batchId, msg.batchTrace);
+				} else if (node.function === 'batchTraceUpdate') {
+					msg.payload = await node.server.api.updateByCustomerAndInstallationAndBatchAndId(customerId, installationId, batchId, batchTraceId, msg.batchTrace);
+				} else{
+					throw new Error('Unsupported function');
+				}
+				msg.error = null;
+				node.send([msg, null]);
+			} catch (e) {
+				msg.error = e;
+				node.send([null, msg]);
 			}
-			node.send(msg);
 		});
 	}
 
--- ikologik-api/ikologik-batch-type.js
+++ ikologik-api/ikologik-batch-type.js
@@ -1,34 +1,43 @@
+const IkologikApi = require("ikologik-api-nodejs");
+
 module.exports = function (RED) {
 
 	function IkologikBatchTypeNode(config) {
 		RED.nodes.createNode(this, config);
 
-		// Get configuration from node
-		this.customerId = config.batchTypeCustomerId;
-		this.installationId = config.batchTypeInstallationId;
+		// Get configuration
+		this.name = config.name;
+		this.server = RED.nodes.getNode(config.server);
+		this.function = config.function;
+		this.customerId = config.customerId;
+		this.installationId = config.installationId;
 		this.batchTypeId = config.batchTypeId;
 		this.batchTypeName = config.batchTypeName;
-		this.function = config.batchTypeFunction;
-		this.credentials = RED.nodes.getNode(config.batchTypeCredentials);
-		var node = this;
 
-		// Functionality of node
+		// Functionality
+		var node = this;
 		node.on('input', async function (msg) {
-			// If input from node choose input, otherwise get info from message
+			// Get data
 			var customerId = node.customerId ? node.customerId : msg.customerId;
 			var installationId = node.installationId ? node.installationId : msg.installationId;
 			var batchTypeId = node.batchTypeId ? node.batchTypeId : msg.batchTypeId;
 			var batchTypeName = node.batchTypeName ? node.batchTypeName : msg.batchTypeName;
 
-			// Use Ikologik-api for requests
-			// TODO: batchTypeName is hard-coded ! Pay attention when changing to a new batchTypeName
-			const api = await node.credentials.api;
-			if (node.function === 'batchTypeGetByName') {
-				msg.payload = await api.batchType.getByName(customerId, installationId, batchTypeName);
-			} else if (node.function === 'batchTypeList') {
-				msg.payload = await api.batchType.list(customerId, installationId);
+			// Execute
+			try{
+				if (node.function === 'batchTypeGetByName') {
+					msg.payload = await node.server.api.batchType.getByCustomerAndInstallationAndName(customerId, installationId, batchTypeName);
+				} else if (node.function === 'batchTypeList') {
+					msg.payload = await node.server.api.batchType.listByCustomerAndInstallation(customerId, installationId);
+				} else {
+					throw new Error('Unsupported function');
+				}
+				msg.error = null;
+				node.send([msg, null]);
+			} catch (e) {
+				msg.error = e;
+				node.send([null, msg]);
 			}
-			node.send(msg);
 		});
 	}
 
